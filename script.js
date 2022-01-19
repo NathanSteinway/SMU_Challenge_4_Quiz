506cdc0ecc5fac04b6809e43e5c5ff9ca73fa3ad
unknown <steinway.nathan@gmail.com>
2022-01-19 08:38:29 -0600

    granted start button js functionality

---
+++ script.js
@@ -0,0 +1,9 @@
+const startButton = document.getElementById('start-btn');
+
+var startTimer = function() {
+
+    console.log('im working!');
+    
+}
+
+startButton.addEventListener('click', startTimer);
